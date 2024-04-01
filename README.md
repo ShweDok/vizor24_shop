00a989210b78c3f246f8562c8bbda659b4ab28da
Evgeniy Nazarov <nazar-ev@ya.ru>
2024-04-01 20:02:50 +0300

    Lesson 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Installation
+ - composer install
+ - create database vizor24_shop vizor24_shop_test
+ - php artisan storage:link
+ - composer require barryvdh/laravel-debugbar --dev (установка debugbar)
+ - 
+
+ 
+# Deploy
+
+
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Connection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Предупреждение об неправильной загрузке из базы - не до конца понял
+        Model::preventLazyLoading(!app()->isProduction());
+        // Предупреждение, если insert и update полей, которых нет в свойстве fillable модели
+        Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+        // Предупреждение, если какой-то из запросов к базе данных выполняется больше, чем 5 секунд
+        DB::whenQueryingForLongerThan(500, function (Connection $connection) {
+            // TODO: В 3 уроке что-то будем делать
+        });
+
+        // TODO: В 3 уроке что-то будем делать с request cycle
     }
 }
